b3194986675b98c994059835b58f30592283fdf9
g1d5ny <jiwon936626@naver.com>
2022-10-09 01:33:22 +0900

    블로그 웹앱 제작하기

---
+++ web_view/lib/screen/home_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  WebViewController? controller;
+  final homeUrl = 'https://jion-developer.tistory.com';
+
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Jion의 개발일지'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                if (controller == null) return;
+                controller!.loadUrl(homeUrl);
+              },
+              icon: Icon(
+                Icons.home,
+              )
+          ),
+        ],
+      ),
+      body: WebView(
+        onWebViewCreated: (WebViewController controller){
+          this.controller = controller;
+        },
+        initialUrl: homeUrl,
+        javascriptMode: JavascriptMode.unrestricted,    // javascript 실행 가능 (ex. youtube url)
+      )
+    );
+  }
+}
